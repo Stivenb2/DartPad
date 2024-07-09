38eb6219874a488fda16cb3cbb2ade629c3c9c99
Stivenb2 <167477878+Stivenb2@users.noreply.github.com>
2024-07-09 17:29:17 -0500

    Create Variables.dart
    Variables usando dart

---
+++ Variables.dart
@@ -0,0 +1,149 @@
+//EJERCICIO #1 
+void main(){
+  String nombre ="Stiven Ricardo Beltran Sanchez";
+  String correo = "stivenbeltran2066@gmail.com";
+ print(nombre + "  " + correo);
+}
+
+//EJERCICIO #2
+void main(){
+  String nombre ="Stiven Ricardo Beltran Sanchez";
+  String correo = "stivenbeltran2066@gmail.com";
+ print(nombre + correo);
+}
+
+//EJERCICIO #3
+void main(){
+  String nombre ="Stiven Ricardo Beltran Sanchez";
+  String correo = "stivenbeltran2066@gmail.com";
+  print(nombre);
+  print("hello word");
+  print("el correo ingresado es " + correo);
+  print("nombre");  
+}
+
+//EJERCICIO #4
+void main(){
+  String nombre ="Stiven Ricardo Beltran Sanchez";
+  String correo = "stivenbeltran2066@gmail.com";
+  print(nombre + correo);
+  print(nombre + "  " + correo);
+  print("Nombre: " + nombre + " Correo: " + correo);
+}
+
+//EJERCICIO #5
+void main() {
+  int numero1 = 7;
+  int numero2 = 2;
+  int numero3 = 6;
+  print('$numero1$numero2$numero3');
+  print('$numero1$numero3$numero2');
+  print('$numero2$numero1$numero3');
+  print('$numero2$numero3$numero1');
+  print('$numero3$numero1$numero2');
+  print('$numero3$numero2$numero1');
+}
+
+
+void main() {
+  int numero1 = 7;
+  int numero2 = 2;
+  int numero3 = 6;
+  print('$numero1$numero2$numero3');
+  print('$numero1$numero3$numero2');
+  print('$numero2$numero1$numero3');
+  print('$numero2$numero3$numero1');
+  print('$numero3$numero1$numero2');
+  print('$numero3$numero2$numero1');
+}
+void main() {
+  int numero1 = 7;
+  int numero2 = 2;
+  int numero3 = 6;
+  print('$numero1$numero2$numero3 - ');
+  print('$numero1$numero3$numero2 - ');
+  print('$numero2$numero1$numero3 - ');
+  print('$numero2$numero3$numero1 - ');
+  print('$numero3$numero1$numero2 - ');
+  print('$numero3$numero2$numero1 - ');
+}
+void main() {
+  int numero1 = 7;
+  int numero2 = 2;
+  int numero3 = 6;
+  print('$numero1$numero2$numero3\n');
+  print('$numero1$numero3$numero2\n');
+  print('$numero2$numero1$numero3\n');
+  print('$numero2$numero3$numero1\n');
+  print('$numero3$numero1$numero2\n');
+  print('$numero3$numero2$numero1\n');
+}
+
+void functionprueba() {
+  print("Esta es la functionprueba");
+}
+
+void main() {
+  functionprueba();
+  double altura = 1.72;
+  int x = 7;
+  String nombre = "Stiven";
+  int X = 8;
+  String pais_persona = "Colombia";
+  String pais = "Mi pais es $pais_persona";
+  int valor = 4542;
+  print("Hola");
+  String correo = "stivenbeltran2006@gmail.com";
+  double y = .04;
+  String ciudad = "Medellin";
+}
+void main() {
+  print("Hola a todos \n");
+  print('Como estan');
+}
+void main() {
+  String ciudad = "Medellin";
+  print(ciudad);
+}
+void main() {
+  int numero = 8714;
+  print(numero);
+}
+void main() {
+  int numero = 2147483647;
+  print(numero);
+}
+void main() {
+  double numero = 214748364.8;
+  print(numero);
+}
+void main() {
+  double numero_decimal = -7.054;
+  print(numero_decimal);
+}
+void main() {
+  bool verdadero = true;
+  print(verdadero);
+  bool falso = false;
+  print(falso);
+}
+void main() {
+  print((true).toString());
+  print((1).toString());
+  print((-2).toString());
+  print(("foo").toString());
+  print((2.3e5).toString());
+  print(([].isEmpty).toString());
+  print(("false").toString());
+}
+void main() {
+  List<String> ciudades = ["medellin", "bogota", "cali", "cartagena"];
+  print(ciudades);
+}
+void main() {
+  String x = "stiven";
+  print(x);
+  x = null;
+  print(x);
+}
+
